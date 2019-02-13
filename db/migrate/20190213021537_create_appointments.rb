ae9acda520e21e9dfa6b7bffcbd7aec034248ceb
Elise Phan <aliceconnic@gmail.com>
2019-02-13 02:16:54 +0000

    Automatically backed up by Learn

--- db/migrate/20190213021537_create_appointments.rb
+++ db/migrate/20190213021537_create_appointments.rb
@@ -1,8 +1,12 @@
 class CreateAppointments < ActiveRecord::Migration
   def change
     create_table :appointments do |t|
-      t.belongs_to 
+      t.belongs_to :doctor
+      t.belongs_to :patient
       t.timestamps null: false
     end
   end
 end
+      t.string :content
+      t.belongs_to :user
+      t.belongs_to :post
