88594173773ba3522e0c788a8137258ba2f82090
Elise Phan <aliceconnic@gmail.com>
2019-02-13 02:53:59 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190213021537) do
-
-  create_table "appointments", force: :cascade do |t|
-    t.datetime "appointment_datetime"
-    t.integer  "doctor_id"
-    t.integer  "patient_id"
-    t.datetime "created_at",           null: false
-    t.datetime "updated_at",           null: false
-  end
+ActiveRecord::Schema.define(version: 20190213021439) do
 
   create_table "doctors", force: :cascade do |t|
     t.string   "name"
