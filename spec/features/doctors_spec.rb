7a327651869315db825de9823370a9f6b365a546
Elise Phan <aliceconnic@gmail.com>
2019-02-13 04:13:37 +0000

    Automatically backed up by Learn

--- spec/features/doctors_spec.rb
+++ spec/features/doctors_spec.rb
@@ -7,7 +7,7 @@ describe "doctors", type: :feature do
     Appointment.create(appointment_datetime: DateTime.new(2016, 01, 11, 20, 20, 0), patient_id: 1, doctor_id: 1)
   end
 
-  #describe "#show page" do
+  describe "#show page" do
     #it "shows all of a doctor's appointment times in a human readable format" do
       #visit doctor_path(@meredith)
       #expect(page).to have_content("January 11, 2016 at 20:20")
